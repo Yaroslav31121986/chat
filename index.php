cea0ae892163bf9dc4cd53abb6097950d52feb2c
Yaroslav31121986 <Footboll777>
2021-12-15 13:58:37 +0200

    practick Socket chat

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Chat</title>
+	<link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+
+<form id="chat" action="">
+    <div class="chat-result" id="chat-result">
+        <input type="text" name="chat-user" id="chat-user" placeholder="Name">
+        <input type="text" name="chat-message" id="chat-message" placeholder="Message">
+        <input type="submit" value="Send">
+    </div>
+</form>
+
+<script src="script/jQuery.js"></script>
+<script src="script/script.js"></script>
+</body>
+</html>
